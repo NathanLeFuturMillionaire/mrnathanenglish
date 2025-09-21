7b0270370aff4c76159a25177dd0072cc2e58baf
Mr Nathan <misterntkofficiel2.0@gmail.com>
2025-09-21 18:43:54 +0200

    Commit

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -1,29 +1,11 @@
 <header class="main-header">
     <div class="container">
         <div class="logo">
-<<<<<<< HEAD
             <a href="./"><strong>Mr Nathan English</strong></a>
-=======
-            <a href="/"><strong>OpenDoorsClass</strong></a>
->>>>>>> b837f2e2fdfe15ffca3ebce918edaaf331d38958
         </div>
 
         <nav class="nav-menu">
             <ul>
-                <li><a href="">Accueil</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle">Examens ▾</a>
-                    <ul class="dropdown-content">
-                        <li><a href="./toefl">TOEFL <small>- Test of English as a Foreign Language</small></a></li>
-                        <li><a href="./ielts">IELTS <small>- International English Language Testing System</small></a></li>
-                        <li><a href="./cambridge">Cambridge English <small>- Certificats Cambridge</small></a></li>
-                        <li><a href="./toeic">TOEIC <small>- Test of English for International Communication</small></a></li>
-                        <li><a href="./pte">PTE Academic <small>- Pearson Test of English Academic</small></a></li>
-                    </ul>
-                </li>
-                <li><a href="./courses">Cours</a></li>
-                <li><a href="./register">Inscription</a></li>
-                <li><a href="./login">Connexion</a></li>
                 <?php if (isset($_SESSION['user']) && $_SESSION['user']['confirmed'] == 1): ?>
                     <!-- Accueil -->
                     <li><a href="./">Accueil</a></li>
@@ -54,7 +36,7 @@
                     <!-- <li><a href="/logout">Déconnexion</a></li> -->
                 <?php else: ?>
                     <!-- Version visiteur -->
-                    <li><a href="/">Accueil</a></li>
+                    <li><a href="./">Accueil</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle">Examens ▾</a>
                         <ul class="dropdown-content">
